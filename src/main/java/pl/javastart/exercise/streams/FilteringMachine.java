25e53169ff753bec6af9e93c86a285f0c17a7232
ToFatToBat <kpapiernia@wp.pl>
2020-08-04 23:55:46 +0200

    poprawione 2 metody Pobierz nieparzyste Pobierz liczbe mneijsza od 20

--- src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachine.java
@@ -1,18 +1,31 @@
 package pl.javastart.exercise.streams;
 
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachine {
 
     public List<Integer> filterOutNotEvenNumbers(List<Integer> numberList) {
-        return null;
+        List<Integer> notEvenNumbers = numberList.stream()
+                .filter(this::isNotEven)
+                .collect(Collectors.toList());
+        return notEvenNumbers;
     }
 
     public List<Integer> filterOutLowerNumbersThan20(List<Integer> numberList) {
-        return null;
+        List<Integer> lowerNumbersThan20 = numberList.stream()
+                .sorted()
+                .filter(this::isLowerThen20)
+                .collect(Collectors.toList());
+        return lowerNumbersThan20;
     }
 
     public List<Book> convertToBooks(List<String> titles) {
+
+
+
+
         return null;
     }
 
@@ -20,4 +33,17 @@ public class FilteringMachine {
     public List<Book> convertToBooksAndReturnOnlyStartingWithGra(List<String> titles) {
         return null;
     }
+
+
+    private boolean isNotEven(Integer number) {
+        return (number % 2 != 0);
+    }
+
+    private boolean isLowerThen20 (Integer number) {
+        return (number < 20);
+    }
 }
+
+
+
+
