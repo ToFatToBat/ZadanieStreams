f3153b886eac18c7e5be9f498c18b7fabac4ae07
ToFatToBat <kpapiernia@wp.pl>
2020-08-05 22:56:48 +0200

    JS ex. 1

---
+++ src/main/java/pl/javastart/exercise/liczbyZeStrumienia/App.java
@@ -0,0 +1,28 @@
+package pl.javastart.exercise.liczbyZeStrumienia;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class App {
+    public static void main(String[] args) {
+
+        numbersStream;
+
+    }
+
+    public List<Integer> numbersStream() {
+        List<Integer> collect = Stream.iterate(8, number -> number + 1)
+                .limit(200)
+                .filter(this::divideByFiveAndGraterThenOneHundret)
+                .map(number -> number * 3)
+                .limit(10)
+                .collect(Collectors.toList());
+
+        return collect;
+    }
+
+    private boolean divideByFiveAndGraterThenOneHundret(Integer number) {
+        return (number > 100 | number % 5 == 0);
+    }
+}
