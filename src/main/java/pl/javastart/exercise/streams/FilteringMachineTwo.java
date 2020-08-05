6ed82910fa4ec9ed5bc13529e3d3c5d7bd523db7
ToFatToBat <kpapiernia@wp.pl>
2020-08-05 22:27:39 +0200

    dodane streamy

--- src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
+++ src/main/java/pl/javastart/exercise/streams/FilteringMachineTwo.java
@@ -2,18 +2,33 @@ package pl.javastart.exercise.streams;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class FilteringMachineTwo {
 
     // metoda powinna zwrócić imiona wszystkich niedorosłych osób (mniej niż 18 lat)
     public List<String> findKidNames(List<Person> ppl) {
 
-
-
-        return new ArrayList<>();
+        List<String> notAdult = ppl.stream()
+                .filter(this::isKid)
+                .map(Person::getName)
+                .collect(Collectors.toList());
+        return notAdult;
     }
 
     public List<User> convertPeopleToUsers(List<Person> people) {
-        return null;
+        List<User> usersList = people.stream()
+                .map(this::convertPersonToUser)
+                .collect(Collectors.toList());
+        return usersList;
+    }
+
+    private boolean isKid(Person person) {
+        return person.getAge() < 18;
+    }
+
+    private User convertPersonToUser (Person person) {
+        User user = new User(person.getName(), person.getAge(), null);
+        return user;
     }
 }
